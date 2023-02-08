44b11d27d883e40f487086e0a6d376e928e13b35
sjkjh2 <sjkjh2@gmail.com>
2023-02-08 17:56:44 +0900

    연산자

--- main.js
+++ main.js
@@ -24,8 +24,40 @@
 // console.log(7 % 5) // 나눈 나머지 값
 
 // 할당 연산자(assignment operator)
-let a = 2
-// a = a + 1
-a /= 1
+// let a = 2
+// // a = a + 1
+// a /= 1
+
+// console.log(a)
+
+
+
+// 비교 연산자(comparison operator)
+// const a = 1
+// const b = 1
+
+// console.log(a === b)
+
+// function isEqual(x, y) {
+//   return x === y
+// }
+
+// console.log(isEqual(1, 1))
+// console.log(isEqual(1, '1'))
+
+// console.log(a <= b)
+
+
+// 논리 연산자(logical operator)
+const a = 1 === 2
+const b = 'AB' === 'AB'
+const c = true
 
 console.log(a)
+console.log(b)
+console.log(c)
+
+console.log('&&: ', a && b && c) // 전부 true면 true 반환
+console.log('||: ', a || b || c) // 하나라도 true면 true 반환
+console.log('!: ', !a) // 반대 값을 반환
+
