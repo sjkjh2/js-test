4957d920e958e7392cc9b4d39ff458818d12c90c
sjkjh2 <sjkjh2@gmail.com>
2023-02-08 15:41:01 +0900

    할당 연산자

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-import getType from './getType'
+// import getType from './getType'
 
-console.log(typeof 'Hello world!!')
-console.log(typeof 123)
-console.log(typeof true)
-console.log(typeof undefined) // 의도 하지 않은 비워진 값
-console.log(typeof null) // 의도한 비워진 값
-console.log(typeof {})
-console.log(typeof [])
+// console.log(typeof 'Hello world!!')
+// console.log(typeof 123)
+// console.log(typeof true)
+// console.log(typeof undefined) // 의도 하지 않은 비워진 값
+// console.log(typeof null) // 의도한 비워진 값
+// console.log(typeof {})
+// console.log(typeof [])
 
-console.log(getType(123))
-console.log(getType(false))
-console.log(getType(null))
-console.log(getType({}))
-console.log(getType([]))
+// console.log(getType(123))
+// console.log(getType(false))
+// console.log(getType(null))
+// console.log(getType({}))
+// console.log(getType([]))
+
+
+
+// 산술 연산자(arithmetic operator)
+// console.log(1 + 2)
+// console.log(5 - 7)
+// console.log(3 * 4)
+// console.log(10 / 2)
+// console.log(7 % 5) // 나눈 나머지 값
+
+// 할당 연산자(assignment operator)
+let a = 2
+// a = a + 1
+a /= 1
+
+console.log(a)
