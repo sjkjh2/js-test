6e6d34b8fa1465b79569e3fe05780d7a3c730dab
sjkjh2 <sjkjh2@gmail.com>
2023-02-08 18:15:10 +0900

    조건문

--- main.js
+++ main.js
@@ -66,12 +66,33 @@
 
 
 // 삼항 연산자(ternary operator)
-const a = 1 < 2
+// const a = 1 < 2
 
-if (a) {
-  console.log('참')
+// if (a) {
+//   console.log('참')
+// } else {
+//   console.log('거짓')
+// }
+
+// console.log(a ? '참' : '거짓')
+
+
+
+
+
+import random from './getRandom'
+
+// 조건문 (If statement)
+
+// console.log(random())
+
+const a =  random()
+if (a === 0) {
+  console.log('a is 0')
+} else if (a === 2) {
+  console.log('a is 2')
+} else if (a === 4) {
+  console.log('a is 4')
 } else {
-  console.log('거짓')
+  console.log('rest...')
 }
-
-console.log(a ? '참' : '거짓')
