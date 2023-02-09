2256806f3aef1f7dd85fd5230e3cbd255ed4be92
sjkjh2 <sjkjh2@gmail.com>
2023-02-09 14:59:41 +0900

    반복문

--- main.js
+++ main.js
@@ -84,22 +84,22 @@ import random from './getRandom'
 
 // 조건문 (If statement)
 
-const a =  random()
+// const a =  random()
 
-switch (a) {
-  case 0: 
-    console.log('a is 0')
-    break
-  case 2:
-    console.log('a is 2')
-    break
-  case 4: 
-    console.log('a is 4')
-    break
+// switch (a) {
+//   case 0: 
+//     console.log('a is 0')
+//     break
+//   case 2:
+//     console.log('a is 2')
+//     break
+//   case 4: 
+//     console.log('a is 4')
+//     break
 
-  default: 
-    console.log('rest...')
-}
+//   default: 
+//     console.log('rest...')
+// }
 
 // if (a === 0) {
 //   console.log('a is 0')
@@ -112,3 +112,18 @@ switch (a) {
 // }
 
 
+// 반복문 (For statement)
+// for (시작조건; 종료조건; 변화조건) {}
+
+const ulEl = document.querySelector('ul')
+console.log(ulEl)
+for (let i = 0; i < 10; i += 1) {
+  const li = document.createElement('li')
+  li.textContent = `list-${i + 1}`
+  if ((i + 1) % 2 === 0) {
+    li.addEventListener('click', function () {
+      console.log(li.textContent)
+    })
+  }
+  ulEl.appendChild(li)
+}
