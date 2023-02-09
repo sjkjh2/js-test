fe83db910ce2e28c164630e318866b5a4dc952e1
sjkjh2 <sjkjh2@gmail.com>
2023-02-09 16:36:25 +0900

    즉시 실행함수

--- main.js
+++ main.js
@@ -180,12 +180,28 @@ import random from './getRandom'
 // 화살표 함수
 // () => {}  vs  function () {}
 
-const double = function (x, y) {
-  return x * 2
-}
-console.log('double: ', double(7))
+// const double = function (x, y) {
+//   return x * 2
+// }
+// console.log('double: ', double(7))
 
 // const doubleArrow = (x, y) => x * 2
 // const doubleArrow = x => x * 2
-const doubleArrow = x => ({name: 'sjkjh2'})
-console.log('doubleArrow: ', doubleArrow(7))
+// const doubleArrow = x => ({name: 'sjkjh2'})
+// console.log('doubleArrow: ', doubleArrow(7))
+
+
+
+// 즉시실행함수
+// IIFE, Immediately-Invoked Function Expression
+
+const a = 7
+function double() {
+  console.log(a * 2)
+}
+double();
+
+// 즉시 실행 함수
+(function () {
+  console.log(a * 2)
+})()
