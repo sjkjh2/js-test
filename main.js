db2a4c333828d550aa7925584ef3580d386db163
sjkjh2 <sjkjh2@gmail.com>
2023-02-09 15:19:15 +0900

    변수 유효범위

--- main.js
+++ main.js
@@ -115,15 +115,27 @@ import random from './getRandom'
 // 반복문 (For statement)
 // for (시작조건; 종료조건; 변화조건) {}
 
-const ulEl = document.querySelector('ul')
-console.log(ulEl)
-for (let i = 0; i < 10; i += 1) {
-  const li = document.createElement('li')
-  li.textContent = `list-${i + 1}`
-  if ((i + 1) % 2 === 0) {
-    li.addEventListener('click', function () {
-      console.log(li.textContent)
-    })
+// const ulEl = document.querySelector('ul')
+// console.log(ulEl)
+// for (let i = 0; i < 10; i += 1) {
+//   const li = document.createElement('li')
+//   li.textContent = `list-${i + 1}`
+//   if ((i + 1) % 2 === 0) {
+//     li.addEventListener('click', function () {
+//       console.log(li.textContent)
+//     })
+//   }
+//   ulEl.appendChild(li)
+// }
+
+
+// 변수 유효범위 (Variable Scope)
+// var, let, const
+
+function scope() {
+  if (true) {
+    const a = 123
+    console.log(a)
   }
-  ulEl.appendChild(li)
 }
+scope()
