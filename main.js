4e922a1bed13f93cd05e6fed3c96c22125dc1997
sjkjh2 <sjkjh2@gmail.com>
2023-02-10 17:34:55 +0900

    타이머 함수

--- main.js
+++ main.js
@@ -222,3 +222,36 @@ import random from './getRandom'
 //   console.log(a * 2)
 // }
 
+
+
+// 타이머 함수
+// setTimeout(함수, 시간): 일정 시간 후 함수 실행
+// setInterval(함수, 시간): 시간 간격마다 함수 실행
+// clearTimeout(): 설정된 Timeout 함수를 종료
+// clearInterval(): 설정된 Interval 함수를 종료
+
+// setTimeout(function () {
+//     console.log('sjkjh2')
+// }, 3000)
+
+// setTimeout(() => {
+//     console.log('sjkjh2')
+// }, 3000)
+
+// const timer = setTimeout(() => {
+//     console.log('sjkjh2')
+// }, 3000)
+
+// const h1El = document.querySelector('h1')
+// h1El.addEventListener('click', () => {
+//     clearTimeout(timer)
+// })
+
+const timer = setInterval(() => {
+    console.log('sjkjh2')
+}, 3000)
+
+const h1El = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+    clearInterval(timer)
+})
